e2423397522491b2850ea26f495638bfec9a2976
ellmo <ellmunzai@gmail.com>
2011-09-27 20:25:32 +0200

    INIT COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ng::Application.config.secret_token = '503512153ca5a562f920c9cb350da7b3183a735d508e9ee941b1e17bcc1d166cca1fcc96aea7a5e34d836f185a0f45e3b82b0655f0254f356f3494beadfb04dc'
